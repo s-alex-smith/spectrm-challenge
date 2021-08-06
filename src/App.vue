a8a952ae5e824c350a48dcacf13278bbd782b252
s-alex-smith <58820910+s-alex-smith@users.noreply.github.com>
2021-08-06 10:55:55 +0100

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,89 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+<div>
+  <h1>"Ich bin ein Berliner!"</h1>
+  <button @click="testlog">Test Log</button>
+  <donut-chart-card :profiles="profiles" />
+  <!-- <DonutChartCard /> -->
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import DonutChartCard from '@/components/DonutChartCard.vue'
 
 export default {
   name: 'App',
+
   components: {
-    HelloWorld
+    DonutChartCard
+  },
+
+
+  methods: {
+    testConsole() {
+      console.log("THIS BETTER FUCKING WORK!");
+    }
+  },
+
+
+  data() {
+    return {
+    "profiles": [
+        {
+            "title": "Gender",
+            "totalLabel": "Total Users",
+            "data": [
+                {
+                    "label": "Female",
+                    "value": 50
+                },
+                {
+                    "label": "Male",
+                    "value": 35
+                },
+                {
+                    "label": "Diverse",
+                    "value": 15
+                }
+            ]
+        },
+        {
+            "title": "Age Range",
+            "totalLabel": "Total Users",
+            "data": [
+                {
+                    "label": "Below 25",
+                    "value": 20
+                },
+                {
+                    "label": "Between 25 and 40",
+                    "value": 67
+                },
+                {
+                    "label": "Above 40",
+                    "value": 13
+                }
+            ]
+        },
+        {
+            "title": "Country",
+            "totalLabel": "Total Users",
+            "data": [
+                {
+                    "label": "Costa Rica",
+                    "value": 22
+                },
+                {
+                    "label": "India",
+                    "value": 37
+                },
+                {
+                    "label": "Canada",
+                    "value": 41
+                }
+            ]
+        }
+    ]
+}
   }
 }
 </script>
---
+++ src/components/DonutChartCard.vue
@@ -0,0 +1,97 @@
+<template>
+  <div>
+<!-- <apexchart width="500" type="donut" :options="chartData.chartOptions" :series="chartData.series"></apexchart> -->
+
+    <div v-for="profile in profiles" :key="profile.title">
+      <div v-for="data in profile.data" :key="data.label">
+        <p>{{data.label}}</p>
+        <p>{{data.value}}</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Vue, { reactive } from "vue";
+import VueApexCharts from 'vue-apexcharts'
+Vue.use(VueApexCharts)
+Vue.component('apexchart', VueApexCharts)
+
+
+
+export default {
+  name: "donut-chart-card",
+  
+  props: {
+    profiles: Array,
+  },
+
+setup() {
+    const chartData = reactive({
+      series: [],
+      chartOptions: {
+        labels: []
+      }
+    })
+
+    return {
+      chartData
+    }
+  },
+
+methods: {
+  testConsole(props) {
+    console.log(props)
+  }
+},
+
+data() {
+  return {
+    // data.value
+    series: [],
+    chartOptions: {
+      // data.label (legend)
+      labels: []
+}
+  }
+}
+
+};
+
+</script>
+ <!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+/* h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+} */
+</style>
+
+
+
+    //   data: function() {
+    //   return {
+    //     chartOptions: {
+    //       chart: {
+    //         id: 'vuechart-example'
+    //       },
+    //       xaxis: {
+    //         categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998]
+    //       }
+    //     },
+    //     series: [{
+    //       name: 'series-1',
+    //       data: [30, 40, 35, 50, 49, 60, 70, 91]
+    //     }]
+    //   }
+    // },  
